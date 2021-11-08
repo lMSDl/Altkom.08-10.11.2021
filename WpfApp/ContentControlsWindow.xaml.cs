2529ec75f21c32733924335d2408ee1e4043630e
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-08 13:32:04 +0100

    Kontrolki

---
+++ WpfApp/ContentControlsWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ContentControlsWindow.xaml
+    /// </summary>
+    public partial class ContentControlsWindow : Window
+    {
+        public ContentControlsWindow()
+        {
+            InitializeComponent();
+            MyTextBlock.Inlines.Add(new Run("underline") { TextDecorations = TextDecorations.Underline });
+            MyTextBlock.Inlines.Add(new Run("bold") { FontWeight = FontWeights.Bold });        
+
+        }
+    }
+}
