74fcf7f3f95e7222a5a4cfe4413a2dd73f64cd35
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-09 13:09:02 +0100

    MarkupExtension, IMultiValueConverter

---
+++ WpfApp/Converters/DateTimeToDaysConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WpfApp.Converters
+{
+    public class DateTimeToDaysConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+            return date.Subtract(DateTime.Now).TotalDays;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var days = (int)value;
+            return DateTime.Now.AddDays(days);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ WpfApp/Converters/StringConcatMultiValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WpfApp.Converters
+{
+    class StringConcatMultiValueConverter : MarkupExtension, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Join(" ", values);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+            //return ((string)value).Split(' ');
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
