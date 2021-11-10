88fed900d8c1652496c810eadea28dec58af6533
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-10 15:58:34 +0100

    Task

--- WpfApp/DiceWindow.xaml.cs
+++ WpfApp/DiceWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,8 +21,12 @@ namespace WpfApp
     /// <summary>
     /// Logika interakcji dla klasy DiceWindow.xaml
     /// </summary>
-    public partial class DiceWindow : Window
+    public partial class DiceWindow : Window, INotifyPropertyChanged
     {
+        private int maximumProgress;
+        private int progress;
+        private bool indeterminateProgress;
+
         public DiceWindow()
         {
             InitializeComponent();
@@ -33,6 +39,35 @@ namespace WpfApp
 
         public int NumberOfDice { get; set; }
         public ObservableCollection<Dice> Results { get; set; }
+        public int MaximumProgress
+        {
+            get => maximumProgress;
+            set
+            {
+                maximumProgress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MaximumProgress)));
+            }
+        }
+        public int Progress
+        {
+            get => progress;
+            set
+            {
+                progress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Progress)));
+            }
+        }
+        public bool IndeterminateProgress
+        {
+            get => indeterminateProgress;
+            set
+            {
+                indeterminateProgress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IndeterminateProgress)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
@@ -44,12 +79,33 @@ namespace WpfApp
         }
         private void Button_Roll_Click(object sender, RoutedEventArgs e)
         {
-            var random = new Random();
-            foreach (var item in Results)
+            var items = Results.Where(x => !x.IsSelected).ToList();
+            //MaximumProgress = items.Count * 100;
+            MaximumProgress = 1;
+            Progress = 0;
+            IndeterminateProgress = true;
+
+            Task.Run(() =>
             {
-                if (!item.IsSelected)
-                    item.Value = random.Next(1, 7);
-            }
+                var random = new Random();
+
+                foreach (var item in Results)
+                {
+                    if (item.IsSelected)
+                        continue;
+
+                    int numberOfRolls = random.Next(25, 75);
+                    for (int i = 0; i < numberOfRolls; i++)
+                    {
+                        item.Value = random.Next(1, 7);
+                        Thread.Sleep(25);
+                        Progress++;
+                    }
+                }
+                Progress = MaximumProgress;
+                IndeterminateProgress = false;
+
+            });
         }
 
         private void Button_Dice_Click(object sender, RoutedEventArgs e)
