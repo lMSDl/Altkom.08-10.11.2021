4b0334bb3feb49ecc6846e3249806b112d9b30be
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-10 10:34:25 +0100

    Walidacja modelu

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,32 +1,84 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Models
 {
-    public class Product : NotifyPropertyChanged, ICloneable
+    public class Product : NotifyPropertyChanged, ICloneable, IDataErrorInfo, INotifyDataErrorInfo
     {
         private string name;
         private DateTime expirationDate;
 
+
+        private Dictionary<string, string> _errorDictionary = new Dictionary<string, string>();
+
+        #region IDataErrorInfo
+        public string this[string columnName] => _errorDictionary.TryGetValue(columnName, out var value) ? value : null;
+        public string Error => string.Join("\n", _errorDictionary.Select(x => x.Value));
+        #endregion IDataErrorInfo
+
+        # region INotifyDataErrorInfo
+        public bool HasErrors => _errorDictionary.Count > 0;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorDictionary.Where(x => x.Key == propertyName).Select(x => x.Value);
+        }
+        #endregion INotifyDataErrorInfo
+
+
+        //Exception Validation - podczas bindingu w XAML ustawiamy ValidatesOnExceptions
         public string Name
         {
             get => name;
             set
             {
-                name = value;
+                if (value.Length > 30 || value.Length < 1)
+                {
+                    throw new ArgumentException("Name is too long. Must have 1-30 characters");
+                }
+                else
+                {
+                    name = value;
+                }
                 OnPropertyChanged();
             }
         }
+
+        //IDataErrorInfo/INotifyDataErrorInfo - podczas bindingu w XAML ustawiamy odpowiednio ValidatesOnDataErrors/ValidatesOnNotifyDataErrors
         public DateTime ExpirationDate
         {
             get => expirationDate;
             set
             {
-                expirationDate = value;
+                if (value < DateTime.Now)
+                {
+                    _errorDictionary[nameof(ExpirationDate)] = "Date must be in future.";
+                }
+                else
+                {
+                    _errorDictionary.Remove(nameof(ExpirationDate));
+                    expirationDate = value;
+                }
+                OnErrorsChanged();
                 OnPropertyChanged();
             }
         }
+
+        private void OnErrorsChanged([CallerMemberName] string propertyName = "")
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            OnPropertyChanged(nameof(HasErrors));
+        }
+
+        //Validation Rules - walidacja całkowicie po stronie XAML
         public decimal Price { get; set; }
+
 
         public object Clone()
         {
---
+++ WpfApp/ValidationRules/ProductPriceValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp.ValidationRules
+{
+    public class ProductPriceValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (decimal.TryParse((string)value, NumberStyles.Number, CultureInfo.InvariantCulture, out var price))
+            {
+                if (price <= 0 || price > 100)
+                {
+                    return new ValidationResult(false, "Price must be between 1-100");
+                }
+            }
+            else
+            {
+                return new ValidationResult(false, "Bad value");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
